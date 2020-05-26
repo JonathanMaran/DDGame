6edfff08719ba0bd1ba3f378ba8d31ea7eef251f
JonathanMaran <jonathan.maran@le-campus-numerique.fr>
2020-05-26 15:03:11 +0200

    exceptions hero choice #2

--- src/main/java/com/campus/warriors/client/console/ClientConsole.java
+++ src/main/java/com/campus/warriors/client/console/ClientConsole.java
@@ -10,14 +10,14 @@ import com.campus.warriors.contracts.WarriorsAPI;
 import com.campus.warriors.engine.Warriors;
 
 public class ClientConsole {
-	
+
 	private static String MENU_COMMENCER_PARTIE = "1";
 	private static String MENU_COMMENCER_PARTIE_MODE_DEBUG = "2";
 	private static String MENU_QUITTER = "3";
 
 	public static void main(String[] args) {
-				
-		
+
+
 		Scanner sc = new Scanner(System.in);
 		String menuChoice = "";
 		do {
@@ -38,7 +38,7 @@ public class ClientConsole {
 		System.out.println();
 		System.out.println("Entrez votre nom:");
 		String playerName = sc.nextLine();
-		
+
 		System.out.println("Choisissez votre héro:");
 		for(int i = 0; i < warriors.getHeroes().size(); i++) {
 			Hero heroe = warriors.getHeroes().get(i);
@@ -46,43 +46,64 @@ public class ClientConsole {
 			System.out.println("    Force d'attaque : " + heroe.getAttackLevel());
 			System.out.println("    Niveau de vie : " + heroe.getLife());
 		}
-		Hero chosenHeroe = warriors.getHeroes().get(Integer.parseInt(sc.nextLine()) - 1);
-		
-		System.out.println("Choisissez votre map:");
-		for(int i = 0; i < warriors.getMaps().size(); i++) {
-			Map map = warriors.getMaps().get(i);
-			System.out.println(i+1 + " - " + map.getName());
-		}
-		Map choosenMap = warriors.getMaps().get(Integer.parseInt(sc.nextLine()) - 1);
-
-		
-		// s'il existe une Game sauvegardée, alors on la reprend, sinon on crée une nouvelle Game
-		GameState gameState = warriors.createGame(playerName, chosenHeroe, choosenMap);
-		String gameId = gameState.getGameId();
-		while (gameState.getGameStatus() == GameStatus.IN_PROGRESS) {
-			System.out.println(gameState.getLastLog());
-			System.out.println("\nAppuyer sur une touche pour lancer le dé"); 
-			if(sc.hasNext()) {
-				sc.nextLine();
-				gameState = warriors.nextTurn(gameId);
-			}									
+	
+		// tant que l'entier n'est pas satisfaisant
+		Hero chosenHero = null;
+		while(chosenHero == null) {
+			try {
+				// saisir un entier
+				chosenHero = warriors.getHeroes().get(Integer.parseInt(sc.nextLine()) - 1);
+				// pour sortir de la boucle
+				// break;
+			} catch (NumberFormatException e) {
+				System.out.println("Entrez un nombre !");
+			} catch(IndexOutOfBoundsException e2) {
+				System.out.println("Entrez un nombre compris entre 1 et "+ warriors.getHeroes().size());
+			} catch (Exception e3) {
+				System.out.println("Erreur inconnue");
+				// send email to admin as an unknown error occured.
+			}
+			
 		}
 		
-		System.out.println(gameState.getLastLog());
-	}
-
-	private static String displayMenu(Scanner sc) {
-		System.out.println();
-		System.out.println("================== Java Warriors ==================");
-		System.out.println("1 - Commencer une partie");
-		System.out.println("2 - Commencer une partie en mode debug");
-		System.out.println("3 - Quitter"); 
-		if(sc.hasNext()) {
-			String choice = sc.nextLine();
-			return choice;
-		}		
 		
-		return "";
-	}
-}
+			
+
+				System.out.println("Choisissez votre map:");
+				for(int i = 0; i < warriors.getMaps().size(); i++) {
+					Map map = warriors.getMaps().get(i);
+					System.out.println(i+1 + " - " + map.getName());
+				}
+
+				Map choosenMap = warriors.getMaps().get(Integer.parseInt(sc.nextLine()) - 1);
+
+				// s'il existe une Game sauvegardée, alors on la reprend, sinon on crée une nouvelle Game
+				GameState gameState = warriors.createGame(playerName, chosenHero, choosenMap);
+				String gameId = gameState.getGameId();
+				while (gameState.getGameStatus() == GameStatus.IN_PROGRESS) {
+					System.out.println(gameState.getLastLog());
+					System.out.println("\nAppuyer sur une touche pour lancer le dé"); 
+					if(sc.hasNext()) {
+						sc.nextLine();
+						gameState = warriors.nextTurn(gameId);
+					}									
+				}
+
+				System.out.println(gameState.getLastLog());
+			}
+
+			private static String displayMenu(Scanner sc) {
+				System.out.println();
+				System.out.println("================== Java Warriors ==================");
+				System.out.println("1 - Commencer une partie");
+				System.out.println("2 - Commencer une partie en mode debug");
+				System.out.println("3 - Quitter"); 
+				if(sc.hasNext()) {
+					String choice = sc.nextLine();
+					return choice;
+				}		
+
+				return "";
+			}
+		}
 
